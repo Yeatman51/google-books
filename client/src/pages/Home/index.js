90100a3345e66a0e6a407ebd13dbd9a970f4f861
jon <jonlyeatman@gmail.com>
2021-06-23 02:11:47 -0400

    Added content to homepage and set up API request

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <div>
-            <h1>HomePage</h1>
+            <h1>Welcome to the ultimate book finder</h1>
+
+            <p>This is the one place where you can search for your favorite book and save it to your custom reading list</p>
+
+            <button>
+            <Link
+              to="/search"
+              className="">Search</Link>
+            </button>
+
+            <button>
+            <Link
+              to="/save"
+              className="">Save</Link>
+            </button>
         </div>
     )
 }
